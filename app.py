883c0ec573894df4c9154c7c9074dbb8b6d5c5b7
SIVA-KRISHNA23 <137750085+SIVA-KRISHNA23@users.noreply.github.com>
2025-10-19 20:04:42 +0530

    debug changed
    Change debug mode to False for production.

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ END:VCARD
 # ----------------- End -----------------
 if __name__ == '__main__':
     app.secret_key = app.config['SECRET_KEY']
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
